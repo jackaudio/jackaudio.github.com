ca76639f1994d1f61876b38ed88802905c39deba
redtide <redtid3@gmail.com>
2020-04-26 02:48:47 +0200

    Updated feed generator and new_post.sh script

--- new_post.sh
+++ new_post.sh
@@ -11,17 +11,18 @@ dir="_posts"
 title="${1}"
 author="${2}"
 date=$(date +%Y-%m-%d)
+datetime=$(date +%Y-%m-%dT%T%z)
 
 # Only numbers, letters and hyphen allowed in a lowercase filename
 name=`echo ${title,,}  | sed 's/ /-/g'`
 name=`echo ${name} | sed 's/[^0-9a-z-]*//g'`
-
 file="${dir}/${date}-${name}.md"
 
 cat <<EOF >${file}
 ---
 title: "${title}"
 author: "${author}"
+date: "${datetime}"
 ---
 EOF
 
